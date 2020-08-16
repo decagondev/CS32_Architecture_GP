07142a9f42102f1bea5d7d852bd6c51c359a698e
Tom Tarpey <tomtarpeydev@gmail.com>
2020-08-16 07:38:03 +0100

    day 1 prep

---
+++ AGENDA.md
@@ -0,0 +1,10 @@
+# Computer Architecture Agenda CS32
+
+## Day 1
+- Overview [I DO]
+- short talk (Problem Solving, number bases review) [I DO]
+- 5 min break
+- DEMO some conversion (in code) [I DO]
+- 5 min break
+- Building a simple Data Driven Machine [WE DO]
+- wrap up
---
+++ day1/simple.py
@@ -0,0 +1,23 @@
+# Let's build a data driven machine!
+
+# What do we need to have our machine working?
+"""
+- Some sort of memory
+- Some way of stopping operation
+- Some way of keeping the CPU running
+- Some sort of storage for local variables sperate from main RAM (memory) eg; Registers
+- Some sort of operations that can be performed such as (printing something, saving data to a variable[register] )
+- Some FETCH, DECODE, EXECUTE CYCLE
+
+"""
+
+# Operations that we can perform
+
+# some sort of memory
+
+# keep track of running?
+
+# Some local var holders [registers]
+
+# REPL to run once per cycle of CPU
+# inside this we will have our FETCH, DECODE, EXECUTE CYCLE
