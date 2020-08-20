0f29bacb92a0b6bfb27aab50a110f8260d37cc4e
Tom Tarpey <tomtarpeydev@gmail.com>
2020-08-20 18:00:00 +0100

    call and ret talk with tangent of C

--- day4/prog.c
+++ day4/prog.c
@@ -1,4 +1,12 @@
-int add(int a, int b) {
+char c = 'A'; // size 1 byte [0101 0101]
+short s = 123; // size 2 bytes [0101 0101 0101 0101]
+int i = 128; // size 4 bytes [0101 0101 0101 0101 0101 0101 0101 0101]
+long int l = 23; // size 8 bytes [0101 0101 0101 0101 0101 0101 0101 0101 0101 0101 0101 0101 0101 0101 0101 0101]
+int x = 120002020;
+
+
+int add(int a, int b)
+{
     return a + b;
 }
 
