61b41defbad88f9704e948be17365c318503f5b0
Tom Tarpey <tomtarpeydev@gmail.com>
2020-08-20 17:30:18 +0100

    branch tables

--- day4/branch_table.py
+++ day4/branch_table.py
@@ -2,11 +2,16 @@
 ADD = 23
 SUB = 34
 PRINT_SOME_STUFF = 50
+SOME_THING = 34
 
 
 def op_add(op1, op2):
     op1 += op2
     return 2
+
+def some_function_i_just_made(op1, op2):
+    print("Doing something here...")
+    return 1
 
 def op_sub(op1, op2):
     op1 -= op2
@@ -19,7 +24,8 @@ def op_print_some_stuff(op1, op2):
 bt = {
     ADD: op_add,
     SUB: op_sub,
-    PRINT_SOME_STUFF: op_print_some_stuff
+    PRINT_SOME_STUFF: op_print_some_stuff,
+    SOME_THING: some_function_i_just_made
 }
 
 """
---
+++ day4/prog.c
@@ -0,0 +1,13 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+float add(float a, float b) {
+    return a + b;
+}
+
+int main() {
+
+    int x = add(34.0f, 34.0f);
+    return 0;
+}
